82716bb510661f08e6e0eac0cc9b7851506d1be3
melanisilva2010-blip <melanisilva2010@gmail.com>
2026-02-14 22:28:59 -0300

    Teleport a escenas

---
+++ Assets/script teleport/PasaLvl8.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PasaLvl8 : MonoBehaviour
+{
+    [SerializeField] private GameObject canvas;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Personaje"))
+        {
+            StartCoroutine(DesvanecerPantalla());
+            SceneManager.LoadScene(9);
+        }
+    }
+    IEnumerator DesvanecerPantalla()
+    {
+        canvas.SetActive(true);
+        yield return new WaitForSeconds(0.1f);
+        canvas.GetComponent<Image>().color = Color.clear;
+        yield return new WaitForSeconds(0.6f);
+    }
+}
+
